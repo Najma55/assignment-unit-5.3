0da38e6c909bbc6f088ffe6348ef1da648dca686
Najma Jama <Najma55.nj@gmail.com>
2023-08-09 10:05:11 -0500

    created a function also created and object and added 6 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,23 @@
 console.log('***** Music Collection *****')
-let Collection =[];
+let collection =[];
+function addToCollection(title, artist, yearPublished) {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    }
+    collection.push(album);
+    return album;
+}
+addToCollection('The Writings On The Wall', 'Destinys Child', 1999);
+console.log(collection[collection.length -1]);
+addToCollection('Love and Life', 'Mary J Blige', 2003);
+console.log(collection[collection.length -1]);
+addToCollection('Trouble', 'Akon', 2004);
+console.log(collection[collection.length -1]);
+addToCollection('Silver Side up' , 'Nickelback' , 2001);
+console.log(collection[collection.length -1]);
+addToCollection('Comin From Where Im From' , 'Anthony Hamilton' , 2003);
+console.log(collection[collection.length -1]);
+addToCollection('Acid Rap' , 'Chance The Rapper' , 2013);
+console.log(collection[collection.length -1]);
