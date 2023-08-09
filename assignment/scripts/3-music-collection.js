7f9a146bc78f2d9a2ac4ec58d61a78a25f809ab4
Najma Jama <Najma55.nj@gmail.com>
2023-08-09 11:34:45 -0500

    created a function that takes an array parameter

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -23,3 +23,11 @@ addToCollection('Acid Rap' , 'Chance The Rapper' , 2013);
 console.log(collection[collection.length -1]);
 
 console.log(collection);
+
+function showCollection(params) {
+    console.log('Number of albums in collection,(params.length)');
+    for(let album of params){
+        console.log('(album.title) by (album.artist), published in (album.yearPublished)');
+    }
+}showCollection(collection);
+
